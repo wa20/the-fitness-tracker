b6f98ab3e3e9714cbd5319fc0194d1fa0a408c5b
wael <78425538+wa20@users.noreply.github.com>
2021-07-02 19:04:55 +0100

    added additional paths to views js

--- routes/views.js
+++ routes/views.js
@@ -2,14 +2,23 @@ const router = require('express').Router();
 const path = require('path');
 
 
+//index
+router.get('/index', (req,res) => {
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+
+//exercise 
 router.get('/exercise', (req, res) =>{
 
     res.sendFile(path.join(__dirname,"../public/exercise.html"))
 })
 
-
-//exercise 
 //stats
+router.get('/stats', (req,res) => {
+    res.sendFile(path.join(__dirname,"../public/stats.html"))
+})
+
 
 
 module.exports = router;
