1b1051a9f833463bf49b3cfe77441a9d8547e26a
wael <78425538+wa20@users.noreply.github.com>
2021-07-02 19:10:31 +0100

    added static middleware to server to access styling elements

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(require('./routes/api'));
 app.use(require('./routes/views'));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
