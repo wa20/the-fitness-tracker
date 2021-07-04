ef6009acbd6be6977ee32c75a168c42b0958c50d
wael <78425538+wa20@users.noreply.github.com>
2021-07-04 15:56:02 +0100

    added morgan package to server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 //require in moongoose and express
 const mongoose = require('mongoose'); 
-const express = require('express'); 
+const express = require('express');
+const logger = require('morgan');
 
 
 const PORT = 3000;
 
 const app = express(); 
 
+app.use(logger('dev'));
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 app.use(require('./routes/api'));
