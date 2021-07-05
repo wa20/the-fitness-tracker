50ee4e4432478edd0f05ad25037c3818decb8572
wael <78425538+wa20@users.noreply.github.com>
2021-07-05 18:21:26 +0100

    update and get total duration api routes added

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const Workout = require('../models/workout');
 
 
 //create workout
-router.post('/api/workouts', ({}, res) => {
+router.post('/api/workouts', (req, res) => {
   Workout.create({})
   .then((createdWorkout) => {
     res.json(createdWorkout);
@@ -14,13 +14,13 @@ router.post('/api/workouts', ({}, res) => {
   })
 });
 
-//get workout data total
+//get workout data total duration
 router.get('/api/workouts', (req, res) =>{
-Workout.aggregate([
+Workout.aggregate([{
     $addFields: {
         totalDuration: {$sum: '$exercise.duration'}
     }
-]).then((createdWorkout) => {
+}]).then((createdWorkout) => {
     res.json(createdWorkout)
 })
 .catch( err => {
@@ -30,9 +30,37 @@ Workout.aggregate([
 })
 
 
+//view all workout duration
+router.get('/api/workouts/range', (req, res) =>{
+    Workout.aggregate([{
+        $addFields: {
+            totalDuration: {
+                $sum: '$exercises.duration'
+            }
+        }
+    }]).sort({id: -1})
+    .limit(10)
+    .then((createdWorkout) => {
+        res.json(createdWorkout)
+    })
+    .catch( err => {
+        res.status(400).json(err);
+    })
+});
 
-//update workouts 
-router.put('/api/workouts/range', ({}))
+
+//update workouts
+router.put('/api/workouts/:id', (req,res) => {
+Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}},
+    //Parameter added so updated data is returned instead of original
+    {new: true},
+).then((createdWorkout) => {
+res.json(createdWorkout)
+})
+.catch( err => {
+    res.status(400).json(err);
+})
+});
 
 
 
