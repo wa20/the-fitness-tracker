70327ab1fd4f51980c22d3e514d8b11547c1ad3a
wael <78425538+wa20@users.noreply.github.com>
2021-07-06 01:38:29 +0100

    minor update ot date function

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
     day: {
         type: Date,
-        default: () => new Date(),
+        default: Date.now(),
     },
     exercises: [{
         type: {
